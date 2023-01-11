36a7b58e6eb57d88e08a40980e4fed8867645a5a
Arturo <arturodelosangeles@hotmail.com>
2023-01-11 15:52:41 -0600

    sucrusales

--- resources/views/livewire/sucursales/view.blade.php
+++ resources/views/livewire/sucursales/view.blade.php
@@ -17,7 +17,31 @@
         </div>
     </div>
     <!-- end page title -->
-
+    <div class="modal-header">
+        <h4 class="modal-title" id="createModalLabel">Agregar sucursal</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span wire:click.prevent="cancel()" aria-hidden="true"><i class="fe-x-circle"></i></span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <p class="sub-header">
+            *Campos requeridos.
+        </p>
+        <form>
+            <div class="form-group">
+                <label for="nombre">Nombre*</label>
+                <input wire:model="nombre" id="nombre" type="text" class="form-control" placeholder="Nombre">@error('nombre') <span class="error text-danger">{{ $message }}</span> @enderror
+            </div>
+            <div class="form-group">
+                <label for="direccion">Dirección*</label>
+                <input wire:model="direccion" id="direccion" type="text" class="form-control" placeholder="Dirección">@error('direccion') <span class="error text-danger">{{ $message }}</span> @enderror
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer justify-content-between">
+        <button type="button" wire:click.prevent="cancel()" class="btn btn-danger btn-rounded waves-effect close-btn" data-dismiss="modal"><i class="bx bx-fw bxs-x-circle"></i> Cerrar</button>
+        <button type="button" wire:click.prevent="store()" class="btn btn-success btn-rounded waves-effect close-modal"><i class="bx bx-fw bxs-check-circle"></i> Guardar</button>
+    </div>
     <div class="row justify-content-center">
 
         <div class="col-12">
